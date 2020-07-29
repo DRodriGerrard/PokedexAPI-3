26678d4323ca6d5c1ba9b5323472e83502b4eb8e
David Rodri <drodrigerrard@gmail.com>
2020-07-29 13:15:59 +0100

    pokemon rest & filtered

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../../pokemon.service';
+import { HttpClient } from '@angular/common/http';
+import { Pokemon } from '../../pokemon';
 
 @Component({
   selector: 'app-home',
@@ -7,12 +10,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  pokemon: Pokemon;
+  listPokemon:Pokemon[] = [];
+  finalPokemonList:Pokemon[] = [];
+
+  constructor(private _pokemonS: PokemonService, private _httpS: HttpClient) { }
 
   ngOnInit(): void {
+    this.getAllPokemon();
   }
 
+  async getAllPokemon(){
+    await this._pokemonS.getAllPokemon()
+    .subscribe(response => {
+      response.results.forEach(async pokemonURL =>
+        await this._pokemonS.getAllPokemonData(pokemonURL.url)
+        .subscribe(pokemonData => this.makePokemon(pokemonData))
+      )
+    })
+  }
+
+  makePokemon(pokemonData){
+    this.pokemon = pokemonData;
+    this.listPokemon.push(this.pokemon);
+    this.finalPokemonList = this.listPokemon.sort(function(a,b){
+      if(a.id <b.id) return -1;
+      else if(a.id > b.id) return 1;
+      return 0;
+    })
+  } 
+
   getFilter(dataFilter){
-    console.log(dataFilter);
+    this.finalPokemonList = [];
+    const pokeFilterData = this.listPokemon.slice(dataFilter.start, dataFilter.end);
+
+    if(dataFilter.type != 'All'){
+      pokeFilterData.forEach(pokemon => {
+        const filter = pokemon.types.some(type => type.type.name == dataFilter.type);
+        if(filter){
+          this.finalPokemonList.push(pokemon);
+        }
+      })
+    }
+    else{
+      this.finalPokemonList = pokeFilterData;
+    }
   }
 }
---
+++ src/app/pokemon.ts
@@ -0,0 +1,13 @@
+export interface Pokemon {
+    id: number,
+    name: string,
+    order: number,
+    weight: number,
+    abilities: any[],
+    sprites: any[],
+    moves: any[],
+    types: any[],
+    stats: any[],
+    species: {},
+    urlAreaEncounters: string
+}
